7e6b4f36e8a18a7ae492af4ea0d41af4bc668fb5
Daniel <danielferp2004@example.com>
2023-12-30 14:17:11 +0100

    queda ver porque no funciona easy_dial y dialog

--- fitxers_projecte_esin/easy_dial.cpp
+++ fitxers_projecte_esin/easy_dial.cpp
@@ -79,7 +79,6 @@ easy_dial::node* easy_dial::insereix(node *t, nat i, const string &k, phone ph){
     }
     if (i == k.size()-1 and t != nullptr){
         t->_p = ph;
-        ++size;
     }
     else if (t != nullptr){
         phone p;
@@ -91,17 +90,28 @@ easy_dial::node* easy_dial::insereix(node *t, nat i, const string &k, phone ph){
 easy_dial::easy_dial(const call_registry& R) throw(error){
     vector<phone> v;
     pref_curs = "";
-    size = 0;
-    seg = 0;
+
     indefinit = false;
     
     R.dump(v);
     mergeSortFreq(v);
+    size = v.size();
+
+    double *ptr = new double[v.size()];
+
+    for (int i = 0; i < v.size(); ++i){
+        ptr[i] = v[i].frequencia();
+    }
+
+    arrayFreqs = ptr;
+
+    phoneMAX = v[0];
     int j = 0;
     for(int i = 0; i < v.size(); ++i){
         _arrel = insereix(_arrel, j, v[i].nom(), v[i]);
-        
     }
+
+
 }
 
 easy_dial::node* easy_dial::cpyeasydial(node *p){
@@ -154,17 +164,17 @@ void easy_dial::resVisitat(node *pt) {
 
 string easy_dial::inici() throw(){
     pref_curs = "";
-    seg = 0;
     indefinit = false;
     resVisitat(_arrel);
 
-    if (_arrel != nullptr){ 
+    if (_arrel != nullptr){
         string res;
         node *rec = _arrel;
         while (rec != nullptr){
             res += rec->_c;
             rec = rec->_cen;
         }
+        rec->_visitat = true;
         return res;
     }
 
@@ -191,19 +201,17 @@ void easy_dial::fistring(node *S, string p, node* &pt, int i){//i == 0
     }
 }
 
-void easy_dial::F(node* S, phone &ph) { 
+void easy_dial::F(node* S, phone &ph, node *&aux) { 
     if (S != nullptr){
         if (ph < S->_p and not S->_visitat){
             ph = S->_p;
+            aux = S;
         }
 
-        F(S->_esq, ph);
-        F(S->_cen, ph);
-        F(S->_dre, ph);
+        F(S->_esq, ph, aux);
+        F(S->_cen, ph, aux);
+        F(S->_dre, ph, aux);
 
-        if (ph == S->_p){
-            S->_visitat = true;
-        }
     }
 }
 
@@ -229,17 +237,34 @@ string easy_dial::seguent(char c) throw(error){
 
     fistring(_arrel, pref_curs, pt);
 
+    node *aux = nullptr;
     phone ph;
-    F(pt, ph);
+    F(pt, ph, aux);
+    aux->_visitat = true;
 
-    ++seg;
 
     return ph.nom();
+}
+
+void easy_dial::menorFreqT(node *p, node *&f, phone &min){
+    
+    if (p != nullptr){
+        if (p->_visitat == true and p->_p < min){
+            f = p;
+            min = p->_p;
+        }
+
+        menorFreqT(p->_esq, f, min);
+        menorFreqT(p->_cen, f, min);
+        menorFreqT(p->_dre, f, min);
+
+    }
 }
 
 string easy_dial::anterior() throw(error){
    
     node *pt = nullptr; 
+    node *minT = nullptr;
 
     if (indefinit){
         throw error(ErrPrefixIndef);
@@ -249,19 +274,33 @@ string easy_dial::anterior() throw(error){
         throw error(ErrNoHiHaAnterior);
     }
 
+    fistring(_arrel, pref_curs, pt);
+    phone q = phoneMAX;
+    menorFreqT(pt, minT, q);
+
+   
+    minT->_visitat = false;
+    minT = nullptr;
+   
+
     pref_curs.pop_back();
 
+    if (pref_curs == ""){
+        return phoneMAX.nom();
+    }
+
     fistring(_arrel, pref_curs, pt);
+
 
     if (pt == nullptr){
         indefinit = true;
         throw error(ErrPrefixIndef);
     }
 
-    phone ph;
-    F(pt, ph);
+    phone q2 = phoneMAX;
+    menorFreqT(pt, minT, q2);
 
-    return ph.nom();
+    return minT->_p.nom();
 
 }
 
@@ -280,7 +319,8 @@ nat easy_dial::num_telf() const throw(error){
     }
 
     phone ph;
-    F(pt, ph);
+    node *aux = nullptr;
+    F(pt, ph, aux);
 
     return ph.numero();
 
@@ -352,27 +392,28 @@ void easy_dial::comencen(const string& pref, vector<string>& result) const throw
     mergeSortNom(result);
 }
 
-void easy_dial::totesFreq(node *pt, nat f[], int &i) {
-    if (pt != nullptr) {
-        if (pt->_p.nom() != "") {
-            f[i++] = pt->_p.frequencia();
-        }
 
-        resVisitat(pt->_esq);
-        resVisitat(pt->_cen);
-        resVisitat(pt->_dre);
-    }
-}
 
 double easy_dial::longitud_mitjana() const throw(){
-    nat freqs[size];
-    int i = 0, sum = 0;
-    totesFreq(_arrel, freqs, i);
-
+    double sum = 0;
+   
     for (int i = 0; i < size; i++) {
-        sum += freqs[i];
+        sum += arrayFreqs[i];
     }
 
-    //  ... 
+    double Pr[size];
+
+    for (int i = 0; i < size; i++){
+        Pr[i] = arrayFreqs[i] / sum;   //freqs = Pr(s)
+    }
+
+    double res = 0;
+
+    for (int i = 0; i < size; ++i){
+        res += Pr[i] * i;
+    }
+
+    return res;
+
 
 }
