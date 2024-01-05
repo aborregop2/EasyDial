44a4b3fac4e3d950df7d172839f9dad8e9260ccc
Adrián <132199795+aborregop2@users.noreply.github.com>
2024-01-05 09:28:02 +0100

    Update dialog.cpp

--- fitxers_projecte_esin/dialog.cpp
+++ fitxers_projecte_esin/dialog.cpp
@@ -1,5 +1,19 @@
 #include "dialog.hpp"
 
 void dialog::dialog(easy_dial& easy, const string& input, vector<string>& answers, nat& numtelf) throw(){
+  answers.push_back(easy.inici())
 
+  for (unsigned int i = 0; i < input.size(); ++i) {
+    if (input[i] == phone::ENDCHAR) {
+        answers.push_back(easy.seguent(phone::ENDPREF));
+      }
+      else if (input[i] == phone::DELETE) {
+        answers.push_back(easy.anterior());
+      }
+      else {
+        answers.push_back(easy.seguent(input[i]));
+      }
+    }
+
+    numtelf = easy.num_telf();
 }
